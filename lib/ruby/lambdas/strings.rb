28d619289f4b2deb2323ac08bfae09973fc9e0ce
Rodrigo Serradura <rodrigo@ysimplicity.com>
2018-12-29 16:57:07 -0200

    Use RubyLambdas::Import to refactor the strategy to load and expose functions

--- lib/ruby/lambdas/strings.rb
+++ lib/ruby/lambdas/strings.rb
@@ -1,21 +1,6 @@
 module Strings
+  require_relative "import"
   require_relative "strings/to_export"
 
-  RubyLambdas::Strings::ToExport.tap do |to_export|
-    to_export
-      .constants
-      .each do |function_name|
-        function = to_export.const_get(function_name)
-
-        const_set(
-          function_name,
-          function.arity == 1 ? function : function.curry
-        )
-      end
-  end
-
-  RubyLambdas::Strings::ALIASES
-    .each do |name, new_names|
-      Array(new_names).each { |new_name| const_set(new_name, const_get(name)) }
-    end
+  RubyLambdas::Import.(from: RubyLambdas::Strings, to: self)
 end
