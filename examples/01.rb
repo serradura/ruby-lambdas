78fe5f9e32a20f75507991a7e17fad9dff64d4fc
Rodrigo Serradura <rodrigo@ysimplicity.com>
2018-12-30 09:24:28 -0200

    Add more examples and benchmark them

--- examples/01.rb
+++ examples/01.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 raise 'Wrong Ruby version!' unless RUBY_VERSION >= '2.6.0'
 
 require 'bundler/inline'
@@ -6,7 +8,12 @@ gemfile do
   source 'https://rubygems.org'
 
   gem 'ruby-lambdas', require: 'ruby/lambdas'
+  gem 'benchmark-ips', '~> 2.7', '>= 2.7.2'
 end
+
+############
+# Examples #
+############
 
                                 # -- Alternative syntax --
 Slugify =                       # Slugify =
@@ -20,10 +27,46 @@ slug = Slugify.(' I WILL be a url slug   ') # Slugify.call(' I WILL be a url slu
 p slug # => "i-will-be-a-url-slug"
 
 def slugify(value)
-  String(value)
+  value
     .then(&Strings::Trim)
     .then(&Strings::LowerCase)
     .then(&Strings::ReplaceAll[' ', '-'])
 end
 
 p slugify('    I will be a url SLUG ') # => "i-will-be-a-url-slug"
+
+##############
+# Benchmarks #
+##############
+
+require 'benchmark/ips'
+
+def slugify2(value)
+  value.strip.downcase.gsub(' ', '-')
+end
+
+Benchmark.ips do |x|
+  x.report('method chaining') { slugify2(' I WILL be a url slug   ') }
+
+  x.report('compose (.>>)') { Slugify.(' I WILL be a url slug   ') }
+
+  x.report('pipeline (.then)') { slugify(' I WILL be a url slug   ') }
+
+  x.compare!
+end
+
+# ruby -v examples/01.rb
+# ruby 2.6.0p0 (2018-12-25 revision 66547) [x86_64-darwin17]
+# Warming up --------------------------------------
+#      method chaining    41.639k i/100ms
+#        compose (.>>)    41.931k i/100ms
+#     pipeline (.then)    32.379k i/100ms
+# Calculating -------------------------------------
+#      method chaining    464.961k (± 1.9%) i/s -      2.332M in   5.016985s
+#        compose (.>>)    470.854k (± 1.6%) i/s -      2.390M in   5.077433s
+#     pipeline (.then)    351.887k (± 2.2%) i/s -      1.781M in   5.063616s
+
+# Comparison:
+#        compose (.>>):   470853.9 i/s
+#      method chaining:   464960.6 i/s - same-ish: difference falls within error
+#     pipeline (.then):   351887.1 i/s - 1.34x  slower
--- examples/02.rb
+++ examples/02.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 raise 'Wrong Ruby version!' unless RUBY_VERSION >= '2.6.0'
 
 require 'bundler/inline'
@@ -6,7 +8,12 @@ gemfile do
   source 'https://rubygems.org'
 
   gem 'ruby-lambdas', require: 'ruby/lambdas'
+  gem 'benchmark-ips', '~> 2.7', '>= 2.7.2'
 end
+
+############
+# Examples #
+############
 
 class Slugifier
   def initialize(sub, new_sub)
@@ -15,17 +22,17 @@ class Slugifier
   end
 
   def call(value)
-    String(value)
+    value
       .then(&Strings::Strip)
       .then(&Strings::Downcase)
-      .then(&Strings::GSub[' ', '-'])
+      .then(&Strings::GSub[@sub, @new_sub])
   end
 end
 
-Slugify = Slugifier.new(' ', '-')
+Slugify1 = Slugifier.new(' ', '-')
 
                                         # -- Alternative syntax --
-p Slugify.(' I will Be a urL slug   ') # Slugify.call(' I will Be a urL slug   ')
+p Slugify1.(' I will Be a urL slug   ') # Slugify.call(' I will Be a urL slug   ')
 
 #
 # The code below is the same of the previous.
@@ -38,7 +45,7 @@ class Slugifier2
   end
 
   def call(value)
-    String(value)
+    value
       .then(&method(:trim))
       .then(&method(:lower_case))
       .then(&method(:replace))
@@ -61,5 +68,91 @@ end
 
 Slugify2 = Slugifier2.new(' ', '-')
 
-                                        # -- Alternative syntax --
-p Slugify2.(' I will be a URL slug   ') # Slugify2.call(' I will be a URL slug   ')
+p Slugify2.(' I will be a URL slug   ')
+
+#
+# Use composition instead of pipeline.
+#
+class Slugifier3
+  def initialize(sub, new_sub)
+    @function =
+      Strings::Strip >> Strings::Downcase >> Strings::GSub[sub, new_sub]
+  end
+
+  def call(value)
+    @function.call(value)
+  end
+end
+
+Slugify3 = Slugifier3.new(' ', '-')
+
+p Slugify3.(' I will be a URL slug   ')
+
+#
+# Use only Lambdas instead classes
+#
+Slugifier4 = -> (sub, new_sub) do
+  Strings::Strip >> Strings::Downcase >> Strings::GSub[sub, new_sub]
+end
+
+Slugify4 = Slugifier4.call(' ', '-')
+
+
+p Slugify4.(' I will be a URL slug   ')
+
+##############
+# Benchmarks #
+##############
+
+require 'benchmark/ips'
+
+class Slugifier5
+  def initialize(sub, new_sub)
+    @sub = sub
+    @new_sub = new_sub
+  end
+
+  def call(value)
+    value.strip.downcase.gsub(@sub, @new_sub)
+  end
+end
+
+Slugify5 = Slugifier5.new(' ', '-')
+
+p Slugify5.(' I will be a URL slug   ')
+
+Benchmark.ips do |x|
+  x.report('Slugify1') { Slugify1.(' I WILL be a url slug   ') }
+
+  x.report('Slugify2') { Slugify2.(' I WILL be a url slug   ') }
+
+  x.report('Slugify3') { Slugify3.(' I WILL be a url slug   ') }
+
+  x.report('Slugify4') { Slugify4.(' I WILL be a url slug   ') }
+
+  x.report('Slugify5') { Slugify5.(' I WILL be a url slug   ') }
+
+  x.compare!
+end
+
+# ruby -v examples/02.rb
+# ruby 2.6.0p0 (2018-12-25 revision 66547) [x86_64-darwin17]
+# Warming up --------------------------------------
+#             Slugify1    32.102k i/100ms
+#             Slugify2    18.719k i/100ms
+#             Slugify3    41.726k i/100ms
+#             Slugify4    41.916k i/100ms
+#             Slugify5    41.679k i/100ms
+# Calculating -------------------------------------
+#             Slugify1    353.234k (± 1.6%) i/s -      1.798M in   5.090599s
+#             Slugify2    197.407k (± 3.4%) i/s -    992.107k in   5.031920s
+#             Slugify3    463.481k (± 2.2%) i/s -      2.337M in   5.044143s
+#             Slugify4    470.417k (± 1.6%) i/s -      2.389M in   5.080262s
+#             Slugify5    467.429k (± 1.3%) i/s -      2.376M in   5.083347s
+
+# Comparison:
+#             Slugify4:   470416.6 i/s
+#             Slugify5:   467429.1 i/s - same-ish: difference falls within error
+#             Slugify3:   463481.2 i/s - same-ish: difference falls within error
+#             Slugify1:   353234.4 i/s - 1.33x  slower
+#             Slugify2:   197407.0 i/s - 2.38x  slower
