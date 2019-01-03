0730fe9ac3ecc11b6b4dcbe2b536eaa5d32146a9
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-01-03 00:37:28 -0200

    Add a new example (strict mode)

---
+++ examples/strict_mode/01.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+
+  gem 'ruby-lambdas', require: 'ruby/lambdas'
+end
+
+############
+# Examples #
+############
+
+p ["2", "4", "8"].map(&Numerics + "2") # => ["22", "24", "28"]
+
+# ---
+
+require "ruby/lambdas/strict-mode"
+
+p [2, 4, 8].map(&Numerics + 2) # => [4, 6, 10]
+
+begin
+  p ["2", "4", "8"].map(&Numerics + "2")
+rescue TypeError => e
+ p e # => #<TypeError: "2" must be Numeric>
+end
+
+p ["2 ", " 4", "8 "].map(&Strings::Strip) # => ["2", "4", "8"]
+
+begin
+  p [2, 4, 8].map(&Strings::Strip)
+rescue TypeError => e
+ p e # => #<TypeError: 2 must be a String>
+end
