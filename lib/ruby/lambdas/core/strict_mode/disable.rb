30b9c963881f49bd4e0b7b56e8b0d41e3d9129ec
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-01-03 14:14:28 -0200

    Add a comment to explain the strict mode

--- lib/ruby/lambdas/core/strict_mode/disable.rb
+++ lib/ruby/lambdas/core/strict_mode/disable.rb
@@ -1,3 +1,9 @@
+#
+# These Kernel methods can be overridden by
+# new ones if the strict mode was enabled. e.g:
+#
+#    require "ruby/lambdas/strict-mode"
+#
 [:String!, :Numeric!].each do |name|
   Kernel.remove_method(name) if Kernel.respond_to?(name)
 end
