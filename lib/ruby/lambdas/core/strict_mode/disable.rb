f506dfbd2d6f5ed7f8a1805a86a1766c0e29c682
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-01-15 01:54:12 -0200

    Create in a dynamically approach the disabled version of the strict mode methods

--- lib/ruby/lambdas/core/strict_mode/disable.rb
+++ lib/ruby/lambdas/core/strict_mode/disable.rb
@@ -6,14 +6,6 @@
 #
 [:String!, :Numeric!].each do |name|
   Kernel.remove_method(name) if Kernel.respond_to?(name)
-end
 
-module Kernel
-  def String!(data)
-    data
-  end
-
-  def Numeric!(data)
-    data
-  end
+  Kernel.module_eval { define_method(name) { |data| data } }
 end
